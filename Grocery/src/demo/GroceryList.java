d2e26d91821114d88c61ad7281927a34acd01caa
MatiasStorm <mat-435@hotmail.com>
2020-09-03 11:44:58 +0200

    Add add method to grocerylist class

--- Grocery/src/demo/GroceryList.java
+++ Grocery/src/demo/GroceryList.java
@@ -3,4 +3,12 @@ import GroceryOrderItem;
 
 public class GroceryList {
     private GroceryOrderItem[] orders = new GroceryOrderItem[10];
+
+    public void add(GroceryOrderItem item){
+        if(orders.length() >= 10){
+            return;
+        }
+        orders.add(item);
+    }
+
 }
