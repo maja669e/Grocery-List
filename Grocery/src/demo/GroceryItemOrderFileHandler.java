43717669ed3776ac99680b8c1df0a233ca46fdd0
MatiasStorm <mat-435@hotmail.com>
2020-09-04 09:18:05 +0200

    Add GroceryItemOrderFileHandler

---
+++ Grocery/src/demo/GroceryItemOrderFileHandler.java
@@ -0,0 +1,32 @@
+package demo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GroceryItemOrderFileHandler {
+    File file = new File("groceries.txt");
+
+    public ArrayList<GroceryItemOrder> scanGroceryItemOrdersFromFile(){
+        ArrayList<GroceryItemOrder> itemOrders = new ArrayList<>();
+        try {
+            Scanner scan = new Scanner(file);
+            String name;
+            int quantity;
+            int price;
+            while(scan.hasNext()){
+                name = scan.next();
+                quantity = scan.nextInt();
+                price = scan.nextInt();
+                itemOrders.add(new GroceryItemOrder(name, quantity, price));
+//                scan.nextLine();
+            }
+            return itemOrders;
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not found!");
+
+        }
+        return itemOrders;
+    }
+}
